07e7482fdad567c1952cf4a9886010e0c81ccdeb
Chakshu Gupta <chakshugupta@gmail.com>
2022-08-16 12:31:03 +0200

    Remove unused code

--- assets/js/script.js
+++ assets/js/script.js
@@ -49,11 +49,6 @@ for (let i = 0; i < testimonialsItem.length; i++) {
 
 }
 
-// add click event to modal close button
-modalCloseBtn.addEventListener("click", testimonialsModalFunc);
-overlay.addEventListener("click", testimonialsModalFunc);
-
-
 // page navigation variables
 const navigationLinks = document.querySelectorAll("[data-nav-link]");
 const pages = document.querySelectorAll("[data-page]");
